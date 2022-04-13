a63de2f778d57613703578b3cb9f0574bb5674c8
IGoldenMemories <GallifreyArcadia@gmail.com>
2022-04-13 12:48:15 +0200

    définition ce que l'on va devoir implémenter pour I.A. minimax

---
+++ Prolog/IA_minmax.pl
@@ -0,0 +1,12 @@
+/* Différentes choses à implémenter/décider
+
+  - Générer l’arbre du jeu jusqu’aux états terminaux
+  - Déterminer l’utilité des états terminaux
+  - Remonter l’arbre en appliquant alternativement min et max
+  - évaluer La valeur au noeud racine (laquelle détermine le meilleur coup/la plus haute utilité)
+
+--> Ceci peut se faire par une stratégie en profondeur d’abord.
+*/
+
+%Définition de la profondeur maximale de recherche
+prof_max(2).
