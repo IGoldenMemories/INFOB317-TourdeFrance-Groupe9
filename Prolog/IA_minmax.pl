76c14158d4d273ac18241de95d06a133a8f3fc7a
IGoldenMemories <GallifreyArcadia@gmail.com>
2022-04-14 11:23:56 +0200

    Algorithm minimax heuristic fonction (evaluation) idea

--- Prolog/IA_minmax.pl
+++ Prolog/IA_minmax.pl
@@ -8,5 +8,24 @@
 --> Ceci peut se faire par une stratégie en profondeur d’abord.
 */
 
+%Utiliser la fonction terminale (selon que tous les coureurs aient passé la ligne d'arrivée)
+
+%Fonction d'évaluation
+%idée (minimiser le temps total (condition pour gagner))
+%choisir action qui permet:
+%   - d'arriver le plus vite à la ligne d'arrivée
+%   - de minimiser la distance entre le coureur (de qui c'est le tour) et la ligne d'arrivée)
+
+%Valeur évaluation :
+%   - +10 (temps total plus petit que celui de tous les autres) (gagné)
+%   - +5 (égalité en temps total le plus petit (avec 1 joueur)) (gagné)?
+%   - +2 (égalité  en temps total le plus petit (avec 2 joueurs)) (gagné)?
+%   - +0 (temps total plus grand qu'un autre (perdu))
+
+
+
+
 %Définition de la profondeur maximale de recherche
-prof_max(2).
+
+
+%Calcul du coup optimal.
